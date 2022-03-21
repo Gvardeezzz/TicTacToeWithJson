8e628d83b3b4cb4cc287a2408ca3c2a2242eb8b8
Gvardeezzz <93467736+Gvardeezzz@users.noreply.github.com>
2022-03-22 00:00:08 +0300

    It works

---
+++ src/main/java/DataForSaving.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class DataForSaving {
+    private Player [] players;
+    private ArrayList<Step> steps;
+
+    public Player[] getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(Player[] players) {
+        this.players = players;
+    }
+
+    public ArrayList<Step> getSteps() {
+        return steps;
+    }
+
+    public void setSteps(ArrayList<Step> steps) {
+        this.steps = steps;
+    }
+}
